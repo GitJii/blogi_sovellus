5d0eb8b330861f228900d4f92f9603e3101efdfd
Jaakko <jaska919@hotmail.com>
2018-12-03 20:58:14 +0200

    Tehtävä_4.18
    Sovellukseen pystyy kirjaamaan POST pyynnöllä osoitteeseen /api/login

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose
   .connect(config.mongoUrl)
@@ -25,6 +26,7 @@ app.use(middleware.logger)
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.error)
 
